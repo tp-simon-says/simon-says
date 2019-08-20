91cc2cd89ed3917814169c62dfcc78a59c75a4e6
Jonathan Azali <azali.jonathan@gmail.com>
2019-08-20 15:27:34 -0700

    Need to implement AI simulated clicks

--- index.js
+++ index.js
@@ -1,24 +1,43 @@
-// Start Button Functions
-document.getElementById("start-button").addEventListener("click", function(){
-    console.log('Start Game!');
-});
+const numArray = [1, 2, 3, 4];
 
-// Top Left Button Functions
-document.getElementById("top-left-button").addEventListener("click", function(){
-    console.log('Top Left Button Pressed!');
-});
+function generateArray(tempArray) {
+  tempArray.push(Math.ceil(Math.random() * 4));
+  return tempArray;
+}
 
-// Top Right Button Functions
-document.getElementById("top-right-button").addEventListener("click", function(){
-    console.log('Top Right Button Pressed!');
-});
+function echoPattern(tempArray) {
+  for (let i = 0; i < tempArray.length; i++) {
+    sleep(2000);
+    switch (tempArray[i]) {
+      default:
+        console.log('error');
+        break;
+      case 1:
+        pressButton('blue');
+        break;
+      case 2:
+        pressButton('red');
+        break;
+      case 3:
+        pressButton('green');
+        break;
+      case 4:
+        pressButton('yellow');
+        break;
+    }
+  }
+}
 
-// Bottom Left Button Functions
-document.getElementById("bottom-left-button").addEventListener("click", function(){
-    console.log('Bottom Left Button Pressed!');
-});
+generateArray(numArray);
+console.log(numArray);
+echoPattern(numArray);
 
-// Bottom Right Button Function
-document.getElementById("bottom-right-button").addEventListener("click", function(){
-    console.log('Bottom Right Button Pressed!');
-});
+function unpressButton(buttonId) {
+  const temp = document.getElementById(buttonId);
+  temp.style.backgroundColor = buttonId;
+}
+
+function pressButton(buttonId) {
+  const temp = document.getElementById(buttonId);
+  temp.style.backgroundColor = 'gray';
+}
