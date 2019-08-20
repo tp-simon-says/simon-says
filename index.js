78c8b528b8b99bf7037125d0718d236db82a5960
jonazali <azali.jonathan@gmail.com>
2019-08-19 22:14:06 -0700

    directories initialized and skeleton code implemented

---
+++ index.js
@@ -0,0 +1,24 @@
+// Start Button Functions
+document.getElementById("start-button").addEventListener("click", function(){
+    console.log('Start Game!');
+});
+
+// Top Left Button Functions
+document.getElementById("top-left-button").addEventListener("click", function(){
+    console.log('Top Left Button Pressed!');
+});
+
+// Top Right Button Functions
+document.getElementById("top-right-button").addEventListener("click", function(){
+    console.log('Top Right Button Pressed!');
+});
+
+// Bottom Left Button Functions
+document.getElementById("bottom-left-button").addEventListener("click", function(){
+    console.log('Bottom Left Button Pressed!');
+});
+
+// Bottom Right Button Function
+document.getElementById("bottom-right-button").addEventListener("click", function(){
+    console.log('Bottom Right Button Pressed!');
+});
