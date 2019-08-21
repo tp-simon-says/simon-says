5e03ce7e621cc60642ae45302f9c4043431653f7
jonazali <azali.jonathan@gmail.com>
2019-08-20 23:06:52 -0700

    single button flash works

--- index.js
+++ index.js
@@ -5,39 +5,63 @@ function generateArray(tempArray) {
   return tempArray;
 }
 
-function echoPattern(tempArray) {
-  for (let i = 0; i < tempArray.length; i++) {
-    sleep(2000);
-    switch (tempArray[i]) {
-      default:
-        console.log('error');
-        break;
-      case 1:
-        pressButton('blue');
-        break;
-      case 2:
-        pressButton('red');
-        break;
-      case 3:
-        pressButton('green');
-        break;
-      case 4:
-        pressButton('yellow');
-        break;
-    }
-  }
-}
 
-generateArray(numArray);
-console.log(numArray);
-echoPattern(numArray);
+function pressButton(buttonId) {
+  const temp = document.getElementById(buttonId);
+  temp.style.backgroundColor = 'gray';
+}
 
 function unpressButton(buttonId) {
   const temp = document.getElementById(buttonId);
   temp.style.backgroundColor = buttonId;
 }
 
-function pressButton(buttonId) {
-  const temp = document.getElementById(buttonId);
-  temp.style.backgroundColor = 'gray';
+function echoPattern(tempArray) {
+  for (let i = 0; i < tempArray.length; i++) {
+    switch (tempArray[i]) {
+      default:
+        console.log('error');
+        break;
+      case 1:
+        playButton('blue');
+        break;
+      case 2:
+        playButton('red');
+        break;
+      case 3:
+        playButton('green');
+        break;
+      case 4:
+        playButton('yellow');
+        break;
+    }
+  }
 }
+
+function flashButton(color){
+  pressButton(color);
+  setTimeout(function(){ unpressButton(color); }, 2000);
+}
+
+/*var i = 1;                     //  set your counter to 1
+
+function myLoop () {           //  create a loop function
+   setTimeout(function () {    //  call a 3s setTimeout when the loop is called
+      alert('hello');          //  your code here
+      i++;                     //  increment the counter
+      if (i < 10) {            //  if the counter < 10, call the loop function
+         myLoop();             //  ..  again which will trigger another 
+      }                        //  ..  setTimeout()
+   }, 3000)
+}
+
+myLoop();                      //  start the loop */
+
+
+generateArray(numArray);
+console.log(numArray);
+
+flashButton('blue');
+
+// echoPattern(numArray);
+
