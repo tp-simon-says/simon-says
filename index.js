35fc87d44551c3381340bdf6bd3f0c2c7fdb46aa
Jonathan Azali <azali.jonathan@gmail.com>
2019-08-21 13:26:12 -0700

    AI implement

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const numArray = [1, 2, 3, 4];
+/* eslint-disable func-names */
+const numArray = [1, 2, 3, 4, 3, 2, 1];
 
 function generateArray(tempArray) {
   tempArray.push(Math.ceil(Math.random() * 4));
   return tempArray;
 }
-
 
 function pressButton(buttonId) {
   const temp = document.getElementById(buttonId);
@@ -16,52 +16,55 @@ function unpressButton(buttonId) {
   temp.style.backgroundColor = buttonId;
 }
 
-function echoPattern(tempArray) {
-  for (let i = 0; i < tempArray.length; i++) {
-    switch (tempArray[i]) {
-      default:
-        console.log('error');
-        break;
-      case 1:
-        playButton('blue');
-        break;
-      case 2:
-        playButton('red');
-        break;
-      case 3:
-        playButton('green');
-        break;
-      case 4:
-        playButton('yellow');
-        break;
-    }
+function echoPattern(num) {
+  switch (num) {
+    default:
+      console.log('error');
+      break;
+    case 1:
+      flashButton('blue');
+      break;
+    case 2:
+      flashButton('red');
+      break;
+    case 3:
+      flashButton('green');
+      break;
+    case 4:
+      flashButton('yellow');
+      break;
   }
 }
 
-function flashButton(color){
+function flashButton(color) {
   pressButton(color);
-  setTimeout(function(){ unpressButton(color); }, 2000);
+  setTimeout(function() {
+    unpressButton(color);
+  }, 500);
 }
 
-/*var i = 1;                     //  set your counter to 1
+let i = 0; //  set your counter to 1
 
-function myLoop () {           //  create a loop function
-   setTimeout(function () {    //  call a 3s setTimeout when the loop is called
-      alert('hello');          //  your code here
-      i++;                     //  increment the counter
-      if (i < 10) {            //  if the counter < 10, call the loop function
-         myLoop();             //  ..  again which will trigger another 
-      }                        //  ..  setTimeout()
-   }, 3000)
+// const numArray = [1, 2, 3, 4];
+function myLoop(tempArray) {
+  //  create a loop function
+  setTimeout(function() {
+    //  call a 3s setTimeout when the loop is called
+    echoPattern(tempArray[i]); //  your code here
+    console.log('press');
+    i++; //  increment the counter
+    if (i < tempArray.length) {
+      //  if the counter < 10, call the loop function
+      myLoop(tempArray); //  ..  again which will trigger another
+    } //  ..  setTimeout()
+  }, 750);
 }
 
-myLoop();                      //  start the loop */
-
+myLoop(numArray); //  start the loop
 
 generateArray(numArray);
 console.log(numArray);
 
-flashButton('blue');
+// flashButton('blue');
 
 // echoPattern(numArray);
-
