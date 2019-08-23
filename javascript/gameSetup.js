87ca865e245f4646baf814a20c4afe98aa8dabf9
Roberto Sanchez <rsanchez92@live.com>
2019-08-22 22:23:04 -0700

    Cleaned Up some code

--- javascript/gameSetup.js
+++ javascript/gameSetup.js
@@ -1,5 +1,7 @@
 /* eslint-disable import/no-mutable-exports */
-import { setupCPU } from './gameplayCPU.js';
+import {
+	setupCPU
+} from './gameplayCPU.js';
 
 export let difficulty = 0;
 export let numArray = [];
@@ -7,45 +9,45 @@ export let ifLose = false;
 export let pos = 0;
 
 export const selectDifficulty = val => {
-  difficulty = val;
+	difficulty = val;
 };
 
 export const startGame = id => {
-  // Change the UI for the Button to have three buttons
-  id.innerHTML = `<div id="groupButtons">
+	// Change the UI for the Button to have three buttons
+	id.innerHTML = `<div id="groupButtons">
   <img id="difficulty-buttons1" class="diff" src="./images/classic.png" />
   <img id="difficulty-buttons2" class="diff" src="./images/hard.png" />
   <img id="difficulty-buttons3" class="diff" src="./images/insane.png" />
 </div>`;
-  console.log(id);
-  document.getElementById('difficulty-buttons1').addEventListener(
-    'click',
-    e => {
-      e.stopImmediatePropagation();
-      selectDifficulty(1);
-      id.innerHTML = `<div></div>`;
-      setupCPU(numArray, difficulty);
-    },
-    true
-  );
-  document.getElementById('difficulty-buttons2').addEventListener(
-    'click',
-    e => {
-      e.stopImmediatePropagation();
-      selectDifficulty(2);
-      id.innerHTML = `<div></div>`;
-      setupCPU(numArray, difficulty);
-    },
-    true
-  );
-  document.getElementById('difficulty-buttons3').addEventListener(
-    'click',
-    e => {
-      e.stopImmediatePropagation();
-      selectDifficulty(3);
-      id.innerHTML = `<div></div>`;
-      setupCPU(numArray, difficulty);
-    },
-    true
-  );
+	console.log(id);
+	document.getElementById('difficulty-buttons1').addEventListener(
+		'click',
+		e => {
+			e.stopImmediatePropagation();
+			selectDifficulty(1);
+			id.innerHTML = `<div></div>`;
+			setupCPU(numArray, difficulty);
+		},
+		true
+	);
+	document.getElementById('difficulty-buttons2').addEventListener(
+		'click',
+		e => {
+			e.stopImmediatePropagation();
+			selectDifficulty(2);
+			id.innerHTML = `<div></div>`;
+			setupCPU(numArray, difficulty);
+		},
+		true
+	);
+	document.getElementById('difficulty-buttons3').addEventListener(
+		'click',
+		e => {
+			e.stopImmediatePropagation();
+			selectDifficulty(3);
+			id.innerHTML = `<div></div>`;
+			setupCPU(numArray, difficulty);
+		},
+		true
+	);
 };
--- javascript/gameplay.js
+++ javascript/gameplay.js
@@ -1,85 +1,88 @@
 /* eslint-disable func-names */
-import {setupCPU } from './gameplayCPU.js';
+import {
+	setupCPU
+} from './gameplayCPU.js';
 const sound = new Audio();
 
 export function playGame(tempArray, loseStatus) {
-  if (loseStatus) {
+	if (loseStatus) {
 
-  } else {
-    setupCPU(tempArray);
-    let i = 0;
-    const result = userTurn(tempArray, i);
-    if (result === false) {
-      loseStatus = true;
-      playGame(tempArray, loseStatus);
-    } else {
-      console.log('Play AGAIN');
-      playGame(tempArray, loseStatus);
-    }
-  }
+	} else {
+		setupCPU(tempArray);
+		let i = 0;
+		const result = userTurn(tempArray, i);
+		if (result === false) {
+			loseStatus = true;
+			playGame(tempArray, loseStatus);
+		} else {
+			console.log('Play AGAIN');
+			playGame(tempArray, loseStatus);
+		}
+	}
 }
 export function userTurn(gameArray, position, didLose, id, diff) {
-  
- 
-  console.log(
-    `Values: ${gameArray}, ${
+
+
+	console.log(
+		`Values: ${gameArray}, ${
       gameArray.length
     }, and ${position}, ${didLose}, and id ${id}`
-  );
-  if (
-    gameArray[position] === getInput(id) &&
-    position === gameArray.length - 1
-  ) {
-    setupCPU(gameArray, diff);
-    position = 0;
-    return position;
-  } else if (gameArray[position] === getInput(id)) {
-    console.log(`match`);
-    
-    position++;
-    return position;
-  } else {
-    console.log(`Mismatch`);
-    lose(gameArray.length);
-    return -1;
-  }
+	);
+	if (
+		gameArray[position] === getInput(id) &&
+		position === gameArray.length - 1
+	) {
+		setupCPU(gameArray, diff);
+		position = 0;
+		return position;
+	} else if (gameArray[position] === getInput(id)) {
+		console.log(`match`);
+
+		position++;
+		return position;
+	} else {
+		console.log(`Mismatch`);
+		lose(gameArray.length);
+		return -1;
+	}
 }
+
 function lose(rounds) {
-  sound.src = "../sounds/gameover.wav"
-  sound.play();
-  console.log(`You lose: YOUR HIGH SCORE:\n ${rounds}\n Play again?`);
-  // Function index resets everything
-  document.getElementById(
-    'start-button'
-  ).innerHTML = `
+	sound.src = "../sounds/gameover.wav"
+	sound.play();
+	console.log(`You lose: YOUR HIGH SCORE:\n ${rounds}\n Play again?`);
+	// Function index resets everything
+	document.getElementById(
+		'start-button'
+	).innerHTML = `
   <img class="diff" src="../images/lose.png"/>
   \n Score:${rounds} \n Play Again?\n [Click Here]`;
-  // play sound
+	// play sound
 }
 
 export function getInput(id) {
-  switch (id) {
-    default:
-      return error;
-    case 'blue':
-        sound.src = "./sounds/1.wav";
-        sound.play();
-      return 1;
-    case 'red':
-        sound.src = "./sounds/2.mp3";
-        sound.play();
-      return 2;
-    case 'green':
-        sound.src = "./sounds/6.wav";
-        sound.play();
-      return 3;
-    case 'yellow':
-        sound.src = "./sounds/4.wav";
-        sound.play();
-      return 4;
-  }
+	switch (id) {
+		default:
+			return error;
+		case 'blue':
+			sound.src = "./sounds/1.wav";
+			sound.play();
+			return 1;
+		case 'red':
+			sound.src = "./sounds/2.mp3";
+			sound.play();
+			return 2;
+		case 'green':
+			sound.src = "./sounds/6.wav";
+			sound.play();
+			return 3;
+		case 'yellow':
+			sound.src = "./sounds/4.wav";
+			sound.play();
+			return 4;
+	}
 }
 
 export function testGame() {
-  console.log('game start');
+	console.log('game start');
 }
